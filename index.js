0f954a590ea6455f96194fede6552935aaa65ac8
Michael Ridgway <mridgway@yahoo-inc.com>
2015-02-03 13:48:12 -0800

    Fix reference to mixin

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
  */
 module.exports = require('./lib/FluxibleApp');
 module.exports.FluxibleContext = require('./lib/FluxibleContext');
-module.exports.Mixin = require('./mixins/StoreMixin');
+module.exports.Mixin = require('./mixins/FluxibleMixin');
