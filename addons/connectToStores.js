722c5ccdbb900c7f1fe3dc729e46c53947a08e38
Antoine Hérault <antoine.herault@gmail.com>
2015-05-05 11:21:50 +0200

    Get state from stores when props change

--- addons/connectToStores.js
+++ addons/connectToStores.js
@@ -39,26 +39,29 @@ module.exports = function connectToStores(Component, stores, getStateFromStores)
                 this.context.getStore(Store).removeChangeListener(this._onStoreChange);
             }, this);
         },
-        getStateFromStores: function () {
+        componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+            this.setState(this.getStateFromStores(nextProps));
+        },
+        getStateFromStores: function (props) {
             if ('function' === typeof getStateFromStores) {
                 var storeInstances = {};
                 stores.forEach(function (store) {
                     var storeName = store.name || store.storeName || store;
                     storeInstances[storeName] = this.context.getStore(store);
                 }, this);
-                return getStateFromStores(storeInstances, this.props);
+                return getStateFromStores(storeInstances, props);
             }
             var state = {};
             //@TODO deprecate?
             Object.keys(getStateFromStores).forEach(function (storeName) {
                 var stateGetter = getStateFromStores[storeName];
                 var store = this.context.getStore(storeName);
-                objectAssign(state, stateGetter(store, this.props));
+                objectAssign(state, stateGetter(store, props));
             }, this);
             return state;
         },
         _onStoreChange: function onStoreChange() {
-            this.setState(this.getStateFromStores());
+            this.setState(this.getStateFromStores(this.props));
         },
         render: function render() {
             return React.createElement(Component, objectAssign({}, this.props, this.state));
