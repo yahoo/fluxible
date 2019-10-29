f3935b9691a8d9ba70279c5fe14b04d3e3733e9b
Lingyan Zhu <lingyan@verizonmedia.com>
2019-10-29 14:31:38 -0700

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   "env": {
+    "browser": true,
     "es6": true,
+    "mocha": true,
     "node": true
   },
   "extends": ["eslint:recommended", "plugin:react/recommended"],
@@ -18,8 +20,16 @@ module.exports = {
     "indent": [2, 4, {"SwitchCase": 1}],
     "no-console": 0,
     "no-empty": 0,
+    "no-prototype-builtins": 0,
     "no-redeclare": 0,
     "no-unused-vars": 0,
-    "quotes": [2, "single"]
+    "quotes": [2, "single"],
+    "react/prop-types": 0
+  },
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "16.0"
+    }
   }
 }
