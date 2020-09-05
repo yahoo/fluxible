1381a0fe7027c462cd339cbb14498601abe0b0a4
Pablo Palacios <pablo.palacios@holidaycheck.com>
2020-09-05 12:15:59 +0200

    fluxible-addons-react: migrate provideContext to use ES2015+ features

--- packages/fluxible-addons-react/src/provideContext.js
+++ packages/fluxible-addons-react/src/provideContext.js
@@ -4,63 +4,15 @@
  */
 'use strict';
 
-var React = require('react');
-var PropTypes = require('prop-types');
-var hoistNonReactStatics = require('hoist-non-react-statics');
-var inherits = require('inherits');
-
-function createComponent(Component, customContextTypes) {
-    var componentName = Component.displayName || Component.name || 'Component';
-    var childContextTypes = Object.assign({
-        executeAction: PropTypes.func.isRequired,
-        getStore: PropTypes.func.isRequired
-    }, customContextTypes || {});
-
-    function ContextProvider(props, context) {
-        React.Component.apply(this, arguments);
-        this.wrappedElementRef = React.createRef();
-    }
-
-    inherits(ContextProvider, React.Component);
-
-    ContextProvider.displayName = 'contextProvider(' + componentName + ')';
-    ContextProvider.propTypes = {
-        context: PropTypes.object.isRequired
-    };
-    ContextProvider.childContextTypes = childContextTypes;
-
-    Object.assign(ContextProvider.prototype, {
-        getChildContext: function () {
-            var childContext = {
-                executeAction: this.props.context.executeAction,
-                getStore: this.props.context.getStore
-            };
-            if (customContextTypes) {
-                Object.keys(customContextTypes).forEach(function (key) {
-                    childContext[key] = this.props.context[key];
-                }, this);
-            }
-            return childContext;
-        },
-
-        render: function () {
-            var props = (Component.prototype && Component.prototype.isReactComponent)
-                ? {ref: this.wrappedElementRef}
-                : null;
-            return React.createElement(Component, Object.assign({}, this.props, props));
-        }
-    });
-
-    hoistNonReactStatics(ContextProvider, Component);
-
-    return ContextProvider;
-}
+const React = require('react');
+const PropTypes = require('prop-types');
+const hoistNonReactStatics = require('hoist-non-react-statics');
 
 /**
  * Provides context prop to all children as React context
  *
  * Example:
- *   var WrappedComponent = provideContext(Component, {
+ *   const WrappedComponent = provideContext(Component, {
  *       foo: PropTypes.string
  *   });
  *
@@ -69,6 +21,49 @@ function createComponent(Component, customContextTypes) {
  * @param {object} customContextTypes Custom contextTypes to add
  * @returns {React.Component} or {Function} if using decorator pattern
  */
-module.exports = function provideContext(Component, customContextTypes) {
-    return createComponent.apply(null, arguments);
-};
+function provideContext(Component, customContextTypes) {
+    class ContextProvider extends React.Component {
+        constructor(props) {
+            super(props);
+            this.wrappedElementRef = React.createRef();
+        }
+
+        getChildContext() {
+            const childContext = {
+                executeAction: this.props.context.executeAction,
+                getStore: this.props.context.getStore
+            };
+            if (customContextTypes) {
+                Object.keys(customContextTypes).forEach(key => {
+                    childContext[key] = this.props.context[key];
+                });
+            }
+            return childContext;
+        }
+
+        render() {
+            const props = (Component.prototype && Component.prototype.isReactComponent)
+                ? {ref: this.wrappedElementRef}
+                : null;
+            return React.createElement(Component, {...this.props, ...props});
+        }
+    }
+
+    ContextProvider.childContextTypes = {
+        executeAction: PropTypes.func.isRequired,
+        getStore: PropTypes.func.isRequired,
+        ...customContextTypes
+    };
+
+    ContextProvider.propTypes = {
+        context: PropTypes.object.isRequired
+    };
+
+    ContextProvider.displayName = `contextProvider(${Component.displayName || Component.name || 'Component'})`;
+
+    hoistNonReactStatics(ContextProvider, Component);
+
+    return ContextProvider;
+}
+
+module.exports = provideContext;
