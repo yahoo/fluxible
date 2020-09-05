9d93880f44465846489bf1f264218459259aaae4
Pablo Palacios <pablo.palacios@holidaycheck.com>
2020-09-05 17:08:44 +0200

    babel: upgraded babel and related packages to their latest versions

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,8 @@
+const path = require('path');
+
 module.exports = {
     recursive: true,
     reporter: 'spec',
-    require: 'babel-register',
+    require: path.join(__dirname, './testSetup'),
     timeout: 10000
-}
+};
