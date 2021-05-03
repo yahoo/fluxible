7d0dd134d462c3843a6ab7c03d202ce9b7193de3
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-05-03 17:38:55 +0200

    fluxible-addons-react: improve packaging (#683)

--- packages/fluxible-addons-react/src/FluxibleComponent.js
+++ packages/fluxible-addons-react/src/FluxibleComponent.js
@@ -2,21 +2,18 @@
  * Copyright 2015, Yahoo Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
-import { Component, cloneElement, createElement } from 'react';
+import { cloneElement, createElement } from 'react';
 import { object, node } from 'prop-types';
 import { FluxibleProvider } from './FluxibleContext';
 
-class FluxibleComponent extends Component {
-    render() {
-        const { children, context } = this.props;
-        const childrenWithContext = cloneElement(children, { context });
-        return createElement(FluxibleProvider, { context }, childrenWithContext);
-    }
-}
+const FluxibleComponent = ({ children, context }) => {
+    const childrenWithContext = cloneElement(children, { context });
+    return createElement(FluxibleProvider, { context }, childrenWithContext);
+};
 
 FluxibleComponent.propTypes = {
     children: node.isRequired,
-    context: object.isRequired
+    context: object.isRequired,
 };
 
 export default FluxibleComponent;
