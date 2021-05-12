d03a10e70b8d0bf004a6ae0456fc5b3ed25244a2
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-05-13 00:45:28 +0200

    generator: update generated app dependencies (#690)

--- packages/generator-fluxible/app/templates/components/Html.js
+++ packages/generator-fluxible/app/templates/components/Html.js
@@ -2,29 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ApplicationStore from '../stores/ApplicationStore';
 
-function Html(props) {
-    return (
-        <html>
-            <head>
-                <meta charSet="utf-8" />
-                <title>{props.context.getStore(ApplicationStore).getPageTitle()}</title>
-                <meta name="viewport" content="width=device-width, user-scalable=no" />
-                <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
-            </head>
-            <body>
-                <div id="app" dangerouslySetInnerHTML={{__html: props.markup}}></div>
-                <script dangerouslySetInnerHTML={{__html: props.state}}></script>
-                <script src={'/public/js/' + props.clientFile}></script>
-            </body>
-        </html>
-    );
-}
+const Html = ({ context, markup, state, clientFile }) => (
+    <html>
+        <head>
+            <meta charSet="utf-8" />
+            <title>{context.getStore(ApplicationStore).getPageTitle()}</title>
+            <meta
+                name="viewport"
+                content="width=device-width, user-scalable=no"
+            />
+            <link
+                rel="stylesheet"
+                href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"
+            />
+        </head>
+        <body>
+            <div id="app" dangerouslySetInnerHTML={{ __html: markup }}></div>
+            <script dangerouslySetInnerHTML={{ __html: state }}></script>
+            <script src={`/public/${clientFile}`}></script>
+        </body>
+    </html>
+);
 
 Html.propTypes = {
     clientFile: PropTypes.string,
     context: PropTypes.object,
     markup: PropTypes.string,
-    state: PropTypes.string
+    state: PropTypes.string,
 };
 
 export default Html;
