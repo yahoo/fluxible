029a02977b8abe021ff57bebe343f5253e437f4e
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-10-31 00:26:22 +0200

    test(create-reduce-store): replace mocha/chai/sinon with jest

--- packages/fluxible-reducer-store/tests/unit/lib/createReducerStore.test.js
+++ packages/fluxible-reducer-store/tests/unit/lib/createReducerStore.test.js
@@ -1,51 +1,54 @@
 /*global describe,it,beforeEach */
 
-import { expect } from 'chai';
-import MessageStore from '../../fixtures/stores/MessageStore';
-import SingleReducerStore from '../../fixtures/stores/SingleReducerStore';
-import messages from '../../fixtures/data/messages';
-import { createMockActionContext } from 'fluxible/utils';
+const MessageStore = require('../../fixtures/stores/MessageStore');
+const SingleReducerStore = require('../../fixtures/stores/SingleReducerStore');
+const messages = require('../../fixtures/data/messages');
+const { createMockActionContext } = require('fluxible/utils');
 
 describe('fluxible-reducer-store', () => {
-    var context;
+    let context;
+
     beforeEach(() => {
         context = createMockActionContext({
             stores: [MessageStore, SingleReducerStore],
         });
     });
+
     it('should receive messages', () => {
         const messageStore = context.getStore(MessageStore);
         const singleStore = context.getStore(SingleReducerStore);
-        expect(messageStore).to.be.instanceof(MessageStore);
+        expect(messageStore).toBeInstanceOf(MessageStore);
         context.dispatch('RECEIVE_MESSAGES', messages);
         const threadMessages = messageStore.getAllForThread('t_1');
-        expect(singleStore.getState().count).to.equal(7);
-        expect(threadMessages).to.be.an('array');
-        expect(threadMessages.length).to.equal(3);
+        expect(singleStore.getState().count).toEqual(7);
+        expect(threadMessages).toHaveLength(3);
     });
+
     it('should handle open thread', () => {
         const store = context.getStore(MessageStore);
         const singleStore = context.getStore(SingleReducerStore);
         context.dispatch('RECEIVE_MESSAGES', messages);
         const threadMessages = store.getAllForThread('t_1');
         threadMessages.forEach((message) => {
-            expect(message.isRead).to.equal(false);
+            expect(message.isRead).toEqual(false);
         });
         context.dispatch('OPEN_THREAD', {
             threadID: 't_1',
         });
-        expect(singleStore.getState().count).to.equal(7);
+        expect(singleStore.getState().count).toEqual(7);
         const newThreadMessages = store.getAllForThread('t_1');
         newThreadMessages.forEach((message) => {
-            expect(message.isRead).to.equal(true);
+            expect(message.isRead).toEqual(true);
         });
     });
+
     it('should dehydrate correctly', () => {
         const store = context.getStore(MessageStore);
         context.dispatch('RECEIVE_MESSAGES', messages);
         const data = store.dehydrate();
-        expect(data).to.deep.equal(store.getState());
+        expect(data).toEqual(store.getState());
     });
+
     it('should rehydrate correctly', () => {
         const store = context.getStore(MessageStore);
         context.dispatch('RECEIVE_MESSAGES', messages);
@@ -54,6 +57,6 @@ describe('fluxible-reducer-store', () => {
         const newStore = new MessageStore();
         newStore.rehydrate(data);
         const afterThreadMessages = newStore.getAllForThread('t_1');
-        expect(afterThreadMessages).to.deep.equal(beforeThreadMessages);
+        expect(afterThreadMessages).toEqual(beforeThreadMessages);
     });
 });
