c58541b3a0717ac1192319493da83deb17cc6ac3
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-10-30 21:50:17 +0200

    docs(examples): add a stales props example

---
+++ examples/stale-props/src/App.js
@@ -0,0 +1,13 @@
+import List from './List';
+import Input from './Input';
+
+function App() {
+    return (
+        <div>
+            <Input />
+            <List />
+        </div>
+    );
+}
+
+export default App;
---
+++ examples/stale-props/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FluxibleProvider } from 'fluxible-addons-react';
+import fluxibleApp from './fluxibleApp';
+import App from './App';
+
+const context = fluxibleApp.createContext();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <FluxibleProvider context={context.getComponentContext()}>
+            <App />
+        </FluxibleProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
